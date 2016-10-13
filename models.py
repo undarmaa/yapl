d491c850fc74e0b38875f361cd1d120aa53fb79f
mayo-ya <mayo.yamasaki@gmail.com>
2016-10-13 23:31:38 +0900

    [Mod] README and license txt.

--- models.py
+++ models.py
@@ -1,10 +1,3 @@
-# Copyright 2016 Mayo Yamasaki. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0.
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# ==============================================================================
 import os
 import sqlite3
 
--- yapl.py
+++ yapl.py
@@ -1,10 +1,3 @@
-# Copyright 2016 Mayo Yamasaki. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0.
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# ==============================================================================
 import argparse
 import bz2
 from collections import Counter
